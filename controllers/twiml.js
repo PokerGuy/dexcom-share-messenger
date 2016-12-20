2633d4e062231a39276f7be7181656ecfda009de
Evan <evan.zlotnick@kasasa.com>
2016-12-20 17:06:30 -0600

    still working on phone calls

--- controllers/twiml.js
+++ controllers/twiml.js
@@ -3,13 +3,9 @@ var twilio = require('twilio');
 var dexData = require('../dexData');
 
 exports.sendPhoneCall = function(req, res) {
-    if (twilio.validateExpressRequest(req, process.env.AUTH_TOKEN)) {
         var twiml = new twilio.TwimlResponse();
 
         twiml.say('Please check on ' + process.env.CHILD_NAME + '. Currently at ' + dexData.glucose + ' glucose event is ' + dexData.eventType);
         res.type('text/xml');
         res.send(twiml.toString());
-    } else {
-        res.send('Not valid');
-    }
 };
