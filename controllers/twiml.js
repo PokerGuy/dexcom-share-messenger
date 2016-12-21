d10adcf565b0122f325c5ed4301579b4283a06bf
Evan <evan.zlotnick@kasasa.com>
2016-12-21 16:36:37 -0600

    better voice

--- controllers/twiml.js
+++ controllers/twiml.js
@@ -5,7 +5,10 @@ var dexData = require('../dexData');
 exports.sendPhoneCall = function(req, res) {
         var twiml = new twilio.TwimlResponse();
 
-        twiml.say('Please check on ' + process.env.CHILD_NAME + '. Currently at ' + dexData.glucose + ' glucose event is ' + dexData.eventType);
+        twiml.say('Please check on ' + process.env.CHILD_NAME + '. Currently at ' + dexData.glucose + ' glucose event is ' + dexData.eventType, {
+        voice:'woman',
+        language:'en-gb'
+    });
         res.type('text/xml');
         res.send(twiml.toString());
 };
