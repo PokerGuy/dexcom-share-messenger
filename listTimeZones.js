702b7afaf07b569c5166f5ed3536c262b7a4ba5b
Evan Zlotnick <evan_zlotnick@yahoo.com>
2016-12-28 11:37:12 -0600

    fix to the listTimeZones

--- listTimeZones.js
+++ listTimeZones.js
@@ -1,16 +1,6 @@
 var moment = require('moment-timezone');
 var _ = require('lodash')
-var sorted = _.sortBy(moment.tz.names(), function (name) {
-    return name;
-});
 
-var found = [];
-
-var f = _.filter(sorted, function(tz) {
-    var first = tz.split('/')[0];
-    if (found.indexOf(first) == -1) {
-        found.push(first);
-        return first;
-    }
+var f = _.each(moment.tz.names(), function(tz) {
+        console.log(tz);
 });
-console.log(found);
