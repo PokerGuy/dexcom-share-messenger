b543ee388f015a77fd0c6e05ddd32863d3fc819f
Evan Zlotnick <evan_zlotnick@yahoo.com>
2017-05-27 14:45:26 -0500

    Allowing an acknowledgement to be sent via a phone call.

--- controllers/twiml.js
+++ controllers/twiml.js
@@ -4,11 +4,15 @@ var dexData = require('../dexData');
 
 exports.sendPhoneCall = function(req, res) {
         var twiml = new twilio.TwimlResponse();
-
-        twiml.say('Please check on ' + process.env.CHILD_NAME + '. Currently at ' + dexData.glucose + ' glucose event is ' + dexData.eventType, {
-        voice:'woman',
-        language:'en-gb'
-    });
+        twiml.gather({
+            numDigits: 1,
+            action: process.env.RESPONSE_URL
+        }, function(gatherNode) {
+            gatherNode.say('Please check on ' + process.env.CHILD_NAME + '. Currently at ' + dexData.glucose + ' glucose event is ' + dexData.eventType + '. Press any key to send an acknowledgement of this alert.', {
+                voice:'woman',
+                language:'en-gb'
+            });
+        });
         res.type('text/xml');
         res.send(twiml.toString());
 };
